96be31d3181385424733ee3f90071448be0fa32f
Evgeny Mymrin <universal.max.pro@gmail.com>
2023-10-04 15:11:32 +0300

    Add project files.

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using ChatClient.ServiceChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IServiceChatCallback
+    {
+        bool isConnected = false;
+        ServiceChatClient client;
+        int id;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChatClient(new InstanceContext(this));
+                id = client.Connect(tbUserName.Text);
+                tbUserName.IsEnabled = false;
+                bConnectDisconnect.Content = "Disconnect";
+                isConnected = true;
+            }
+        }
+
+        void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(id);
+                client = null;
+                tbUserName.IsEnabled = true;
+                bConnectDisconnect.Content = "Connect";
+                isConnected = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(isConnected)
+            {
+                DisconnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MessageCallback(string message)
+        {
+            lbChat.Items.Add(message);
+            lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count - 1]);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (client != null)
+                {
+                    client.SendMessage(tbMessage.Text, id);
+                    tbMessage.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ Chat_WCF/ServerUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace Chat_WCF
+{
+    public class ServerUser
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public OperationContext OperationContext { get; set; }
+
+    }
+}
